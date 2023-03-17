68f3399abda5f07c3d071829550eb8234d296751
adam-handke <adam.handke@outlook.com>
2023-03-17 23:59:53 +0100

    add: Python Arcade init, minor readme adjustments

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # Blockade
-Python remake of [a 1976 multiplayer snake-like game](https://en.wikipedia.org/wiki/Blockade_(video_game)). Includes 4 types of AI bots:
+Python remake of [a 1976 multiplayer snake-like game](https://en.wikipedia.org/wiki/Blockade_(video_game)) created in [Python Arcade](https://api.arcade.academy/en/latest/).
+
+The game currently supports only 2 players. Humans can use arrows or WSAD. There are 4 types of AI bots:
 - **Random** - randomly selects one of the possible move directions (avoids immediate death until it's inevitable).
 - **Heuristic** - always chases the opponent while dodging immediate death.
 - **Optimized** - the best bot achieved in an evolutionary optimization process.
 - **Reinforcement learning** - bot trained from many games as a policy.
+
+The player which survives wins!
 
 ## Requirements
 
@@ -16,19 +20,23 @@ python blockade.py [-h]
                    [-p1 {arrows,wsad,random,heuristic,optimized,rl}]
                    [-p2 {arrows,wsad,random,heuristic,optimized,rl}]
                    [-a {10,11,12,13,14,15,16,17,18,19,20}]
-                   [-s {1,2,3,4,5,6,7,8,9}] 
+                   [-t {25,30,35,40,45,50,55,60,65,70,75}]
+                   [-s GAME_SPEED] 
+                   [-w]
                    [-v]
 ```
 
 Arguments:
   - `-h`, `--help` - show help message and exit;
-  - `-p1` `--player1` - type of the first player (arrows, wsad, random, heuristic, optimized, rl};
-  - `-p2`, `--player2` - type of the second player (arrows, wsad, random, heuristic, optimized, rl};
-  - `-a`, `--arena-size` - size of the square game arena (10-20);
-  - `-s`, `--game-speed` - game speed, number of moves per second (1-9);
-  - `-v`, `--verbose` - verbose switch, prints game info to the terminal.
+  - `-p1` `--player1` - type of the first player (`arrows`, `wsad`, `random`, `heuristic`, `optimized`, `rl`), default: `arrows`;
+  - `-p2`, `--player2` - type of the second player (`arrows`, `wsad`, `random`, `heuristic`, `optimized`, `rl`), default: `random`;
+  - `-a`, `--arena-size` - size of the square game arena (in tiles, `10`-`20`), default: `10`;
+  - `-t`, `--tile-size` - size of a square game tile (in pixels, `25`-`75`), default: `50`;
+  - `-s`, `--game-speed` - game speed, number of moves per second (positive float), default: `2.0`;
+  - `-w`, `--window-hidden` - hides game window, default: `False`;
+  - `-v`, `--verbose` - verbose switch, prints game info to the terminal, default: `False`.
 
-Note: human players can't use the same input method (arrows, wsad).
+Note: two human players can't use the same input method at the same time.
 
 ## Bot performance comparison
 
--- blockade.py
+++ blockade.py
@@ -1,24 +1,47 @@
+import numpy as np
+
 from players.HumanPlayer import HumanPlayer
 from players.RandomBot import RandomBot
 from players.HeuristicBot import HeuristicBot
 from players.OptimizedBot import OptimizedBot
 from players.ReinforcementLearningBot import ReinforcementLearningBot
 import argparse
+import arcade
 
 
-class Blockade:
-    def __init__(self, player1, player2, arena_size, game_speed, verbose):
+class Blockade(arcade.Window):
+    def __init__(self, player1, player2, arena_size, tile_size, game_speed, window_hidden, verbose):
+        # arcade init
+        # size of the window depends on the arena and tile size but is limited by the screen resolution
+        max_width = min(arena_size * tile_size, int(arcade.window_commands.get_display_size()[0]))
+        max_height = min(arena_size * tile_size, int(arcade.window_commands.get_display_size()[1]))
+        self.actual_window_size = min(max_width, max_height)
+        super().__init__(width=self.actual_window_size, height=self.actual_window_size, title='Blockade',
+                         visible=not window_hidden)
+        self.background_color = arcade.color.ARSENIC
+
+        # command line arguments init
         self.player1 = player1
         self.player2 = player2
         self.arena_size = arena_size
+        self.tile_size = tile_size
         self.game_speed = game_speed
         self.verbose = verbose
 
-    def __str__(self):
-        description = f'Game of Blockade with parameters:\n'
-        for i, (key, value) in enumerate(self.__dict__.items()):
-            description += f'\t{key} = {value}' + (',\n' if i < len(self.__dict__)-1 else '')
-        return description
+        # game init
+        self.game_matrix = None
+        self.frame_counter = None
+
+    def setup(self):
+        # game preparation
+        # TODO
+        self.game_matrix = np.zeros((self.arena_size, self.arena_size))
+        pass
+
+    def on_draw(self):
+        # main game loop
+        self.clear()
+        # TODO
 
 
 if __name__ == '__main__':
@@ -27,13 +50,16 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-p1', '--player1', help='type of the first player',
-                        choices=player_types.keys(), default='random')
+                        choices=player_types.keys(), default='arrows')
     parser.add_argument('-p2', '--player2', help='type of the second player',
                         choices=player_types.keys(), default='random')
-    parser.add_argument('-a', '--arena-size', help='size of the square game arena', type=int,
+    parser.add_argument('-a', '--arena-size', help='size of the square game arena (in tiles)', type=int,
                         choices=range(10, 21), default=10)
-    parser.add_argument('-s', '--game-speed', help='game speed, number of moves per second', type=int,
-                        choices=range(1, 10), default=2)
+    parser.add_argument('-t', '--tile-size', help='size of a square game tile (in pixels)', type=int,
+                        choices=range(25, 80, 5), default=50)
+    parser.add_argument('-s', '--game-speed', help='game speed, number of moves per second (positive float)',
+                        type=float, default=2.0)
+    parser.add_argument('-w', '--window-hidden', help='hides game window', action='store_true')
     parser.add_argument('-v', '--verbose', help='verbose switch, prints game info to the terminal', action='store_true')
     args = parser.parse_args()
 
@@ -41,6 +67,10 @@ if __name__ == '__main__':
     if player_types[args.player1] == HumanPlayer and player_types[args.player2] == HumanPlayer \
             and args.player1 == args.player2:
         raise ValueError(f'two human players use the same input method: {args.player1}')
+
+    # assert that game speed is larger than zero
+    if args.game_speed <= 0:
+        raise ValueError(f'game speed is not positive: {args.game_speed}')
 
     # create player1
     if player_types[args.player1] == HumanPlayer:
@@ -54,10 +84,18 @@ if __name__ == '__main__':
     else:
         init_player2 = player_types[args.player2](verbose=args.verbose)
 
+    if args.verbose:
+        print(f'Starting a game of Blockade with parameters:', flush=True)
+        for i, (key, value) in enumerate(vars(args).items()):
+            print(f'\t{key} = {value}' + (',\n' if i < len(vars(args)) - 1 else ''), end='', flush=True)
+
     game = Blockade(player1=init_player1,
                     player2=init_player2,
                     arena_size=args.arena_size,
+                    tile_size=args.tile_size,
                     game_speed=args.game_speed,
+                    window_hidden=args.window_hidden,
                     verbose=args.verbose)
 
-    print(game)
+    game.setup()
+    arcade.run()
